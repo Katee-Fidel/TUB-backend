9b5e815efea7d2f462025d7340c71ac25ab9ac89
Katee-Fidel <111779050+Katee-Fidel@users.noreply.github.com>
2026-08-27 23:00:45 +0300

    Phase A: expose savings and refund ledger operations

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -7,6 +7,7 @@ const {
   topupWallet,
   getTopupStatus,
 } = require('../controllers/walletController.js');
+const { contributeSavings, refundTicket } = require('../controllers/ledgerController.js');
 
 const router = express.Router();
 
@@ -14,5 +15,7 @@ router.get('/', requireAuth, getMyWallet);
 router.get('/transactions', requireAuth, getWalletTransactions);
 router.post('/topup', requireAuth, verifyOrigin, topupWallet);
 router.get('/topup/:id/status', requireAuth, getTopupStatus);
+router.post('/savings/contribute', requireAuth, verifyOrigin, contributeSavings);
+router.post('/refund/:ticketId', requireAuth, verifyOrigin, refundTicket);
 
 module.exports = router;
