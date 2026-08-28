7d8455be24819314af1819b0db47187d60e24e1a
Katee-Fidel <111779050+Katee-Fidel@users.noreply.github.com>
2026-08-28 16:56:33 +0300

    Add authenticated wallet transaction history route

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -1,16 +1,17 @@
-// routes/walletRoutes.js
 const express = require('express');
 const { requireAuth } = require('../middleware/auth.js');
 const { verifyOrigin } = require('../middleware/csrf.js');
 const {
-    getMyWallet,
-    topupWallet,
-    getTopupStatus,
+  getMyWallet,
+  getWalletTransactions,
+  topupWallet,
+  getTopupStatus,
 } = require('../controllers/walletController.js');
 
 const router = express.Router();
 
 router.get('/', requireAuth, getMyWallet);
+router.get('/transactions', requireAuth, getWalletTransactions);
 router.post('/topup', requireAuth, verifyOrigin, topupWallet);
 router.get('/topup/:id/status', requireAuth, getTopupStatus);
 
